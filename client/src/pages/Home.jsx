46e4a07d5c171dd83864653a44d263e056dba9c1
Nilesh Tiwari <93751356+Niltiwari7@users.noreply.github.com>
2023-11-21 16:03:17 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -54,12 +54,12 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-[#333333] font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+          Find your  <span className='text-slate-500'>Recipes</span>
           <br />
-          Recipes with ease
+          and create your own Recipes.
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Recipe Adda is the best place to find your next perfect recipe to eat.
+          Recipe Adda is the best place to find your recipe to eat.
           <br />
           We have a wide range of recipes for you to choose from.
         </div>
